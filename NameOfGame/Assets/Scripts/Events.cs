ced8b9b80a470d042d10a33d3091fba7b8dd05c1
TurkeyPuncha <turkeypuncher@gmail.com>
2021-10-10 17:07:14 -0700

    Helper Code
    Add helper functions to assist with event management and string operations.

---
+++ NameOfGame/Assets/Scripts/Events.cs
@@ -0,0 +1,90 @@
+﻿/// <summary>
+/// Events for EventManager
+/// </summary>
+
+/*
+    // Example Event
+    public class ExampleEvent : TEventBase
+    {
+        public GameObject ExamplePayload { get; set; }
+        public int ExampleValue { get; set; }
+        public ExampleEvent(GameObject examplePayload, int value)
+        {
+            ExamplePayload = examplePayload;
+            ExampleValue = value;
+        }
+    }
+
+    // Managing the Listener
+    EventManager.AddListener<TopScoreRecievedEvent>(OnTopScoreRecievedEventHandler);
+    EventManager.RemoveListener<TopScoreRecievedEvent>();
+
+*/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreChangedEvent : TEventBase
+{
+    public int Score { get; set; }
+
+    public ScoreChangedEvent(int score)
+    {
+        Score = score;
+    }
+}
+
+public class RankRecievedEvent : TEventBase
+{
+    public int Rank { get; set; }
+
+    public RankRecievedEvent(int rank)
+    {
+        Rank = rank;
+    }
+}
+
+public class TopScoreRecievedEvent : TEventBase
+{
+    public List<ScoreRecord> TopScores { get; set; }
+
+    public TopScoreRecievedEvent(List<ScoreRecord> topScores)
+    {
+        TopScores = topScores;
+    }
+}
+
+public class PlayerNameChangedEvent : TEventBase
+{
+    public string PlayerName { get; set; }
+
+    public PlayerNameChangedEvent(string playerName)
+    {
+        PlayerName = playerName;
+    }
+}
+
+public class CustomerEvent : TEventBase
+{
+    public bool IsHappy { get; set; }
+
+    public CustomerEvent(bool isHappy)
+    {
+        IsHappy = isHappy;
+    }
+}
+
+public class NewOrderEvent : TEventBase 
+{ 
+}
+
+public class LaunchedIngredientEvent : TEventBase
+{
+    public Transform Ingredient{ get; set; }
+
+    public LaunchedIngredientEvent(Transform xform)
+    {
+        Ingredient = xform;
+    }
+}
+
